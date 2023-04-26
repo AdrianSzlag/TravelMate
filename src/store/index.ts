a9d5f65ce3a711cc90944c169714e55802ef739c
Adrian Szlag <aszlag00@gmail.com>
2023-04-26 10:06:23 +0000

    redux installation and configuration

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import placesReducer from "./places";
+
+const store = configureStore({
+  reducer: { places: placesReducer },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
