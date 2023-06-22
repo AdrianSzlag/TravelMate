5bbaf78739133d4267376aed2816c5fb79b199a2
Adrian Szlag <aszlag00@gmail.com>
2023-06-22 16:19:16 +0000

    Remove book again button from business view

--- frontend/src/features/Reservations/components/List.tsx
+++ frontend/src/features/Reservations/components/List.tsx
@@ -18,6 +18,7 @@ interface ItemProps {
   selected: boolean;
   onClick: () => void;
   onCancel: () => void;
+  bookAgain?: boolean;
 }
 
 const Item = ({
@@ -29,47 +30,48 @@ const Item = ({
   date,
   onClick,
   onCancel,
+  bookAgain = false,
 }: ItemProps) => {
   const dateObj = date ? new Date(date) : undefined;
   const isDone = dateObj ? dateObj.getTime() < Date.now() : false;
   const onCancelClickHandler = () => onCancel();
   return (
     <div
-      className="m-2 rounded-lg shadow border box-border flex"
+      className="m-2 box-border flex rounded-lg border shadow"
       onClick={onClick}
     >
       <div className="flex-1 px-4 py-2">
         {isDone && (
-          <div className="bg-gray-200 text-gray-600 px-2 rounded-3xl text-sm font-semibold inline-block">
+          <div className="inline-block rounded-3xl bg-gray-200 px-2 text-sm font-semibold text-gray-600">
             Ended
           </div>
         )}
-        <h1 className="font-semibold text-lg text-gray-800">{title}</h1>
+        <h1 className="text-lg font-semibold text-gray-800">{title}</h1>
         {address && (
-          <h6 className="font-semibold text-sm text-gray-400">{address}</h6>
+          <h6 className="text-sm font-semibold text-gray-400">{address}</h6>
         )}
-        <div className="flex flex-row items-center mt-2">
+        <div className="mt-2 flex flex-row items-center">
           {image && (
             <Img
               src={"/" + image}
               alt={name}
-              className="w-5 h-5 rounded-full mr-2"
+              className="mr-2 h-5 w-5 rounded-full"
             />
           )}
           <h3 className="font-semibold text-gray-600">{name}</h3>
         </div>
-        <div className="flex mb-1 mt-2">
+        <div className="mb-1 mt-2 flex">
           {!isDone && (
             <button
-              className="px-2 py-0.5 bg-red-500 text-white rounded font-semibold text-sm"
+              className="rounded bg-red-500 px-2 py-0.5 text-sm font-semibold text-white"
               onClick={onCancelClickHandler}
             >
               Cancel
             </button>
           )}
-          {isDone && (
+          {isDone && bookAgain && (
             <Link
-              className="px-2 py-0.5 bg-blue-500 text-white rounded font-semibold text-sm"
+              className="rounded bg-blue-500 px-2 py-0.5 text-sm font-semibold text-white"
               to={`/place/${id}?details=services`}
             >
               Book again
@@ -79,15 +81,15 @@ const Item = ({
       </div>
       {dateObj && (
         <>
-          <div className="border-l flex-grow-0 flex-shrink-0 my-4"></div>
-          <div className="flex-none w-20 flex flex-col justify-center items-center">
-            <div className="text-gray-600 text-sm">
+          <div className="my-4 flex-shrink-0 flex-grow-0 border-l"></div>
+          <div className="flex w-20 flex-none flex-col items-center justify-center">
+            <div className="text-sm text-gray-600">
               {getMonthName(dateObj.getMonth())}
             </div>
-            <div className="text-gray-800 font-semibold text-lg">
+            <div className="text-lg font-semibold text-gray-800">
               {dateObj.getDate()}
             </div>
-            <div className="text-gray-600 text-sm">{getTime(date!)}</div>
+            <div className="text-sm text-gray-600">{getTime(date!)}</div>
           </div>
         </>
       )}
@@ -131,10 +133,10 @@ const List = () => {
 
   return (
     <>
-      <div className="relative h-full w-full max-h-full flex-shrink-0 flex-grow-0 sm:w-[400px]">
+      <div className="relative h-full max-h-full w-full flex-shrink-0 flex-grow-0 sm:w-[400px]">
         <div className="absolute top-0 left-0 bottom-0 right-0  overflow-auto bg-[#fcfcfc]">
           {reservations.length === 0 && (
-            <h1 className="text-gray-600 font-semibold text-lg m-4">
+            <h1 className="m-4 text-lg font-semibold text-gray-600">
               No reservations yet{" "}
               <Link to="/" className="text-blue-600">
                 Book something now {":)"}
@@ -142,7 +144,7 @@ const List = () => {
             </h1>
           )}
           {reservations.length > 0 && (
-            <h1 className="text-gray-800 font-bold m-4">Your reservations:</h1>
+            <h1 className="m-4 font-bold text-gray-800">Your reservations:</h1>
           )}
           {sortedReservations
             .filter(
@@ -161,21 +163,22 @@ const List = () => {
                 date={reservation.date}
                 onClick={() => setSelectedReservation(reservation.id)}
                 onCancel={() => cancelReservationHandler(reservation)}
+                bookAgain={true}
               />
             ))}
           {businesses && businesses.length > 0 && (
             <>
-              <h1 className="text-gray-800 font-bold m-4">
+              <h1 className="m-4 font-bold text-gray-800">
                 Reservations in your businesses:
               </h1>
               {businesses.map((business) => {
                 return (
                   <div key={business.id}>
-                    <div className="flex mx-4 mb-4 items-baseline ">
-                      <h1 className="text-gray-600 font-semibold text-lg mr-2">
+                    <div className="mx-4 mb-4 flex items-baseline ">
+                      <h1 className="mr-2 text-lg font-semibold text-gray-600">
                         {business.name}
                       </h1>
-                      <div className=" text-gray-400 text-sm">
+                      <div className=" text-sm text-gray-400">
                         {business.address}
                       </div>
                     </div>
