193444e4b5edb60324f4c80117e9e4ede15212ab
Adrian Szlag <aszlag00@gmail.com>
2023-08-31 15:55:19 +0000

    Migrate from pigeon-map to react-map-gl

--- frontend/src/features/BusinessModal/components/Map.tsx
+++ frontend/src/features/BusinessModal/components/Map.tsx
@@ -1,23 +1,34 @@
-import { Map as PigeonMap, Marker } from "pigeon-maps";
+import { PiMapPinFill } from "react-icons/pi";
+import Map, { MapLayerMouseEvent, Marker } from "react-map-gl/maplibre";
 
 interface Props {
   coordinates?: [number, number];
   setCoordinates: (coordinates: [number, number]) => void;
 }
 
-const Map = ({ coordinates, setCoordinates }: Props) => {
-  const handleMapClick = ({ latLng }: { latLng: [number, number] }) => {
-    setCoordinates(latLng);
+const SelectMap = ({ coordinates, setCoordinates }: Props) => {
+  const handleMapClick = (event: MapLayerMouseEvent) => {
+    setCoordinates([event.lngLat.lat, event.lngLat.lng]);
   };
   return (
-    <div className="w-full h-[250px]">
-      <PigeonMap center={[0, 0]} zoom={1} onClick={handleMapClick}>
+    <div className="h-[250px] w-full">
+      <Map
+        onClick={handleMapClick}
+        mapStyle="https://api.maptiler.com/maps/basic-v2/style.json?key=u0kFdoythHBvPdgFbgqj"
+        cursor="default"
+      >
         {coordinates && (
-          <Marker anchor={coordinates} payload={1} color="#be3131" />
+          <Marker
+            longitude={coordinates[1]}
+            latitude={coordinates[0]}
+            anchor="bottom"
+          >
+            <PiMapPinFill className="h-8 w-8 cursor-pointer text-red-600 drop-shadow-[0_0_4px_rgba(255,255,255,1)] hover:drop-shadow-[0_0_4px_rgba(0,0,0,0.3)]" />
+          </Marker>
         )}
-      </PigeonMap>
+      </Map>
     </div>
   );
 };
 
-export default Map;
+export default SelectMap;
